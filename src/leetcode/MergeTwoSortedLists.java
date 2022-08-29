c2825c52b29367730b19a9b0210d6a5263b34173
sehun-kim <rogarithm@gmail.com>
2022-08-29 20:38:54 +0900

    필요 없는 주석 삭제. 의문점 관련 주석은 깃허브 이슈 #2 으로 이동

--- src/leetcode/MergeTwoSortedLists.java
+++ src/leetcode/MergeTwoSortedLists.java
@@ -47,24 +47,15 @@ public class MergeTwoSortedLists {
         // System.out.println(test5.next.next.next.val);
         // System.out.println(test5.next.next.next.next.val);
         // System.out.println(test5.next.next.next.next.next.val);
-
-        // System.out.println(new ListNode().next == null);
-        // 빈 ListNode는 어떻게 만들지?
-        // 두 빈 ListNode를 비교할 때 주소값 이외 요소로 비교하려면?
-        // 문자열로 확인하려면 어떻게 해야 하지?
     }
 
-    // new ListNode()로 빈 노드를 만들면 val 값이 0인데 []로 빈 값은 어떤 경우지?
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // null 체크를 어떻게 나누고 뭘 먼저 해야 할까?
-        // 한쪽만 null인 경우, 둘 다 null인 경우
         ListNode result = null;
+        // 두 노드 모두 비었을 경우
         if (list1 == null && list2 == null) {
-            // 두 노드가 비었으면 빈 노드를 반환
             result = null;
         }
-        // 두 노드 중 하나는 뒤에 값이 있다면 값 하나만 있는 노드의 값이 어디 들어갈지 계산해야 한다
-        // 하지만 이 작업은 아래서 해준다.
+        // 두 노드 중 하나만 비었을 경우
         else if (list1 == null || list2 == null) {
             ListNode nonEmptyNode = list1 == null ? list2 : list1;
             result = new ListNode(nonEmptyNode.val, nonEmptyNode.next);
@@ -75,12 +66,9 @@ public class MergeTwoSortedLists {
             int bigger = list1.val <= list2.val ? list2.val : list1.val;
             result = new ListNode(smaller, new ListNode(bigger));
         }
-        // ListNode는 정렬되어 있다. 어떤 값을 더 앞에 붙일지 어떻게 결정할까?
-        // A.val, B.val을 비교해보고, 값이 같으면 한쪽을 next로 바꿔서 비교한다
-        // 어느 한쪽이 작다면, 큰 쪽의 모든 요소는 작은 요소보다 크다
+        // ListNode는 정렬되어 있다. 어느 한쪽이 작다면, 큰 쪽의 모든 요소는 작은 요소보다 크다
         else if (list1.val <= list2.val) {
             // list1.val이 맨 앞에 온다. 남은 건 mergeTwoLists에 넣어 계산한다
-            // mergeTwoLists()를 재귀로 계산하더라도 이전 계산 결과를 유지하려면 어디에 둬야 할까?
             result = new ListNode(list1.val, mergeTwoLists(list1.next, list2));
         }
         else if (list1.val > list2.val) {
