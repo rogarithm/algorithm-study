8c6d15f169452a809a6363788c0bbac51ad44857
sehun-kim <rogarithm@gmail.com>
2022-09-12 00:19:49 +0900

    ( 케이스 실패
    Stack에 (가 들어가면 s가 끝나서 반복문을 나온다. 반복문을 나오면 무조건
    true를 출력해서 틀린 답이 나온다.
    반복문 밖에서도 parenthesesToCheck이 비었는지 체크해 반환값을 정하는 건
    어떨까 생각해봤다.

---
+++ src/leetcode/ValidParentheses.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        ValidParentheses vp = new ValidParentheses();
+        System.out.println("}" + vp.isValid("}") + ": false");
+        System.out.println("{}" + vp.isValid("{}") + ": true");
+        System.out.println("{}{{}}" + vp.isValid("{}{{}}") + ": true");
+        System.out.println("(" + vp.isValid("(") + ": false");
+        System.out.println("{[}" + vp.isValid("{[}") + ": false");
+        System.out.println("[}" + vp.isValid(("[)")) + ": false");
+        System.out.println("{[]}" + vp.isValid("{[]}") + ": true");
+        System.out.println("{}[]" + vp.isValid("{}[]") + ": true");
+        System.out.println("(([]){})" + vp.isValid("(([]){})") + ": true");
+        System.out.println("([]){}" + vp.isValid("([]){}") + ": true");
+        System.out.println("([)]" + vp.isValid("([)]") + ": false");
+        System.out.println("[({(())}[()])]" + vp.isValid("[({(())}[()])]") + ": true");
+        System.out.println("{(())}[()]" + vp.isValid("{(())}[()]") + ": true");
+        System.out.println("({(())}[()])" + vp.isValid("({(())}[()])") + ": true");
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> parenthesesToCheck = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                parenthesesToCheck.push(')');
+            } else if (c == '[') {
+                parenthesesToCheck.push(']');
+            } else if (c == '{') {
+                parenthesesToCheck.push('}');
+            } else if (parenthesesToCheck.isEmpty()) {
+                return false;
+            } else if (!parenthesesToCheck.isEmpty() && parenthesesToCheck.pop() != c) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
