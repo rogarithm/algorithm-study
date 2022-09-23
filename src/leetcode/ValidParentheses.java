a53b2838358fc4b3d234a7f1d07d51d17ca120e7
sehun-kim <rogarithm@gmail.com>
2022-09-23 14:47:49 +0900

    모호한 변수 이름 수정

--- src/leetcode/ValidParentheses.java
+++ src/leetcode/ValidParentheses.java
@@ -21,24 +21,24 @@ public class ValidParentheses {
     }
 
     public boolean isValid(String s) {
-        Stack<Character> parenthesesToCheck = new Stack<>();
+        Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
             if (c == '(') {
-                parenthesesToCheck.push(')');
+                stack.push(')');
             } else if (c == '[') {
-                parenthesesToCheck.push(']');
+                stack.push(']');
             } else if (c == '{') {
-                parenthesesToCheck.push('}');
-            } else if (parenthesesToCheck.isEmpty()) {
+                stack.push('}');
+            } else if (stack.isEmpty()) {
                 // s 안 문자가 홀수개일 경우나 s가 닫는 괄호로 시작하는 경우 짝이 안맞으니 멈추고 false 반환
                 return false;
-            } else if (parenthesesToCheck.pop() != c) {
+            } else if (stack.pop() != c) {
                 // 스택에서 꺼낸 문자가 일치하면 다음 반복문으로 넘어가고 일치하지 않으면 짝이 안맞으니 멈추고 false 반환
                 return false;
             }
         }
 
-        if (parenthesesToCheck.isEmpty()) {
+        if (stack.isEmpty()) {
             return true;
         } else {
             // 여는 괄호 하나만 있을 경우는 반복문 빠져나와도 스택이 비어있지 않지만 짝이 없으니 false를 반환해야 한다
