b51fd7dbc4f247a905f83b12d7fae367428e38a7
sehun-kim <rogarithm@gmail.com>
2022-10-06 15:28:57 +0900

    재귀로 구현
     - 릿코드에서 실행했을 때 timeout 나왔다.
     - 중간 연산 결과를 연산 끝나기 전까지 들고있어야 해서 그런 것 같다.

--- src/leetcode/ClimbStairs.java
+++ src/leetcode/ClimbStairs.java
@@ -25,6 +25,19 @@ public class ClimbStairs {
     }
 
     public int climbStairs(int n) {
-        return 0;
+        // 처음은 무조건 1이나 2. 그 다음엔 가능하냐 불가능하냐에 따라서 1, 2 둘 다 될수도, 1이나 2 중 하나만 될 수도, 아무것도 안될 수도 있다.
+        // 아무것도 안될 가능성을 미리 판단할 수 있는 기준이 있을까?
+
+        // 가능한 모든 경로를 확인해본다.
+        // 지금 체크하는 경로가 유효하면 결과에 1을 더한다. 유효한지 어떻게 확인하나? n이 0이 되면 유효한 경로다.
+        int result = 0;
+
+        if (n == 0 || n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        } else {
+            return climbStairs(n - 1) + climbStairs(n - 2);
+        }
     }
 }
