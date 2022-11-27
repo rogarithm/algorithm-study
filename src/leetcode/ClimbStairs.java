655aaf805e0def2f02c1ff7cccf1c334cb333d8c
sehun-kim <rogarithm@gmail.com>
2022-11-27 15:11:27 +0900

    memoize 방식 적용
     - 계산 결과를 hashmap에 저장해둔다
     - 한 번 계산했던 값은 hashmap에서 꺼내쓰도록 해서 다시 계산하지 않아도
       되도록 한다

--- src/leetcode/ClimbStairs.java
+++ src/leetcode/ClimbStairs.java
@@ -16,7 +16,12 @@
 //2. 1 step + 2 steps
 //3. 2 steps + 1 step
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ClimbStairs {
+
+    private Map<Integer, Integer> memo = new HashMap<>();
 
     public static void main(String[] args) {
         ClimbStairs cs = new ClimbStairs();
@@ -32,14 +37,22 @@ public class ClimbStairs {
 
         // 가능한 모든 경로를 확인해본다.
         // 지금 체크하는 경로가 유효하면 결과에 1을 더한다. 유효한지 어떻게 확인하나? n이 0이 되면 유효한 경로다.
+
         int result = 0;
 
         if (n == 0 || n == 1) {
             return 1;
         } else if (n == 2) {
             return 2;
-        } else {
-            return climbStairs(n - 1) + climbStairs(n - 2);
         }
+
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+
+        result = climbStairs(n - 1) + climbStairs(n - 2);
+        memo.put(n, result);
+
+        return result;
     }
 }
