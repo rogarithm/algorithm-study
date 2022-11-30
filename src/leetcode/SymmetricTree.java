bbed48818941b3922bb0352dfebad50c438737d4
sehun-kim <rogarithm@gmail.com>
2022-11-30 15:27:49 +0900

    초기 환경 설정

---
+++ src/leetcode/SymmetricTree.java
@@ -0,0 +1,33 @@
+public class SymmetricTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return false;
+    }
+
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
+
+
