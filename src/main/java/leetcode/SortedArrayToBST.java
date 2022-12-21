b1f1bf39be367e5578426311aa89d7ee39e53993
sehun-kim <rogarithm@gmail.com>
2022-12-21 22:59:05 +0900

    배열 요소가 홀수 개인 테스트 케이스 작성

--- src/main/java/leetcode/SortedArrayToBST.java
+++ src/main/java/leetcode/SortedArrayToBST.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 // leetcode 108. Convert Sorted Array to Binary Search Tree
 // 입력으로 주어지는 배열은 오름차순으로 정렬되어 있다
 // 배열을 BST로 변환해야 한다
@@ -35,6 +37,14 @@ public class SortedArrayToBST {
 
         // 배열의 요소 갯수가 홀수 개라면, 중간 위치의 값을 root로 하고 root 왼쪽 요소들이 left TreeNode, root 오른쪽
         // 요소들이 right TreeNode가 되는 TreeNode를 만든다
+        int[] testCase4 = {1, 2, 3, 4, 5};
+        TreeNode testResult4 = tester.sortedArrayToBST(testCase4);
+        System.out.println(
+                testResult4.val == 3 && testResult4.left.val == 2
+                && testResult4.left.left.val == 1 && testResult4.right.val == 5
+                && testResult4.right.left.val == 4
+        );
+
     }
 
     public TreeNode sortedArrayToBST(int[] nums) {
