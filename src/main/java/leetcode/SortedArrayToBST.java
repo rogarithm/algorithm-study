32b3a10f67bc202ee78f809c8bb7623617db74b6
sehun-kim <rogarithm@gmail.com>
2022-12-21 23:52:18 +0900

    배열 요소가 홀수 개일 때 통과하도록 구현

--- src/main/java/leetcode/SortedArrayToBST.java
+++ src/main/java/leetcode/SortedArrayToBST.java
@@ -61,8 +61,8 @@ public class SortedArrayToBST {
             int mid = nums.length / 2;
             return new TreeNode(
                     nums[mid],
-                    sortedArrayToBST(Arrays.copyOfRange(nums, 0, mid - 1)),
-                    sortedArrayToBST(Arrays.copyOfRange(nums, mid + 1, nums.length - 1))
+                    sortedArrayToBST(Arrays.copyOfRange(nums, 0, mid)),
+                    sortedArrayToBST(Arrays.copyOfRange(nums, mid + 1, nums.length))
             );
         }
         return null;
