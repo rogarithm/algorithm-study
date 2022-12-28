ac88cc52b6d9143631911b9360982e143a128656
sehun-kim <rogarithm@gmail.com>
2022-12-28 23:09:18 +0900

    문제 풀이 아이디어 추가

--- src/main/java/leetcode/PascalsTriangle.java
+++ src/main/java/leetcode/PascalsTriangle.java
@@ -15,6 +15,10 @@ public class PascalsTriangle {
     }
 
     public List<List<Integer>> generate(int numRows) {
+        /* numRow는 해당 행의 요소 갯수와도 일치한다 */
+        /* 각 행에 들어갈 array list 생성 시 안에 넣을 수 있는 요소 갯수도 정해야 할까? */
+        /* 이전 행의 인덱스를 기준으로 새로 만들려는 행의 요소 값을 계산할 수 있을까? */
+
         ArrayList<List<Integer>> result = new ArrayList<>();
 
         if (numRows == 1) {
