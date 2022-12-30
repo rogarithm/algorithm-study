2a0c53d293d74b989f89a0dacb2e82be0761e713
sehun-kim <rogarithm@gmail.com>
2022-12-30 18:10:38 +0900

    이전 행 번호를 변수로 대체

--- src/main/java/leetcode/PascalsTriangle.java
+++ src/main/java/leetcode/PascalsTriangle.java
@@ -53,7 +53,8 @@ public class PascalsTriangle {
         /* 0 1=3.0+3.1 2=3.1+3.2 3 */
         /* 바로 위 행의 계산 결과를 어떻게 가져오지? */
         if (row == 3) {
-            List<Integer> previous = result.get(2 - 1);
+            int previousIndex = row - 2;
+            List<Integer> previous = result.get(previousIndex);
             ArrayList<Integer> elem = new ArrayList<>();
             elem.add(1);
             elem.add(previous.get(0) + previous.get(1));
