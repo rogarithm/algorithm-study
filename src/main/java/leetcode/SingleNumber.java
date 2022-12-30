e131c6bba39ee8f32297d3623947d8ad79c97c0b
sehun-kim <rogarithm@gmail.com>
2022-12-30 18:28:27 +0900

    초기 환경 설정

---
+++ src/main/java/leetcode/SingleNumber.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+public class SingleNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int singleNumber(int[] nums) {
+        return 1;
+    }
+}
