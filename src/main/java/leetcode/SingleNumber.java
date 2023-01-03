2747ff8a56ed535663d50c6208e246db20780755
sehun-kim <rogarithm@gmail.com>
2023-01-04 01:26:21 +0900

    set으로 구현 변경 후 릿코드 통과
     - hashMap만 쓴다고 가정했을 때, value가 1인 key를 찾을 방법이 막연했다.
     - stack만 쓴다고 가정했을 때, 2233441 같은 식으로 순차적으로 나오는
       경우에만 한 번 나오는 값을 찾을 수 있다.
     - set으로 구현해서 해결했다. set에 add된 요소일 경우 삭제하고 마지막에
       남은 값은 하나일 것이므로 iterator()로 유일한 값을 반환하도록 했다.

--- src/main/java/leetcode/SingleNumber.java
+++ src/main/java/leetcode/SingleNumber.java
@@ -3,9 +3,9 @@ package leetcode;
 // Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 // You must implement a solution with a linear runtime complexity and use only constant extra space.
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class SingleNumber {
 
@@ -27,21 +27,20 @@ public class SingleNumber {
         /* 계산하는 도중에는 출력값이 될 수 있는 요소가 여러 개 될 수 있으므로 이들을 모아두고 두 번 나온 값은 제거해 마지막으로 남은 요소를
         * 출력으로 내보내기 위해 스택을 이용했다. 하지만 스택 가장 위에 있는 값이 아닌 상황이 생겼다. */
 
-        Map<Integer, Integer> cache = new HashMap<>();
-        Stack<Integer> result = new Stack<>();
-
+        Set<Integer> cache = new HashSet<Integer>();
         for (int num = 0; num < nums.length; num++) {
-            int key = nums[num];
-            if (cache.get(key) == null) {
-                cache.put(key, 1);
-                result.push(key);
-            }
-            else if (cache.get(key) == 1) {
-                cache.remove(key);
-                result.pop();
+            int value = nums[num];
+            if (!cache.contains(value)) {
+                cache.add(value);
+            } else if (cache.contains(value)) {
+                cache.remove(value);
             }
         }
+        Iterator<Integer> result = cache.iterator();
+        if (result.hasNext()) {
+            return result.next();
+        }
 
-        return result.peek();
+        return 0;
     }
 }
