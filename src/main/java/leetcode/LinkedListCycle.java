1a9316b1169837e8e07eed5f4fe65d705510dfcf
sehun-kim <rogarithm@gmail.com>
2023-01-06 14:14:45 +0900

    ListNode 길이 구하는 메서드 구현 및 테스트

--- src/main/java/leetcode/LinkedListCycle.java
+++ src/main/java/leetcode/LinkedListCycle.java
@@ -6,26 +6,33 @@ package leetcode;
 // that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 // Return true if there is a cycle in the linked list. Otherwise, return false.
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LinkedListCycle {
 
     public static void main(String[] args) {
         LinkedListCycle tester = new LinkedListCycle();
         ListNode l1 = new ListNode(1);
         System.out.println(tester.hasCycle(l1) == false);
+        System.out.println(tester.getListNodeLength(l1));
 
         ListNode l2 = new ListNode(1);
         ListNode l2Next = new ListNode(2);
         l2.next = l2Next;
         l2Next.next = l1;
         System.out.println(tester.hasCycle(l2) == true);
+        System.out.println(tester.getListNodeLength(l2));
 
         ListNode l3 = new ListNode(1);
         ListNode l3Next = new ListNode(2);
         l3.next = l3Next;
         System.out.println(tester.hasCycle(l3) == false);
+        System.out.println(tester.getListNodeLength(l3));
     }
 
     public boolean hasCycle(ListNode head) {
+        int length = getListNodeLength(head);
         if (head.next == null) // 재귀함수로 만들 경우에도 이 조건이 유효한가? 유효하다. 유효한 순환 LinkedList는 모든 ListNode가 next 필드를 가져야 한다.
             return false;
         // 마지막 요소인지 판별할 수 있는 조건이 있나? 순회하는 요소 목록을 기록해야 할까?
@@ -35,6 +42,29 @@ public class LinkedListCycle {
             return false;
         else
             return true;
+    }
+
+    private int getListNodeLength(ListNode head) {
+        Map<ListNode, Boolean> cache = new HashMap<>();
+        int index = 0;
+
+        if (head != null) {
+            cache.put(head, true);
+            index++;
+        }
+
+        ListNode nextElement = head;
+        while (nextElement.next != null) {
+            nextElement = nextElement.next;
+            if (cache.get(nextElement) == null) {
+                cache.put(nextElement, true);
+                index++;
+            }
+            if (cache.get(nextElement) != null && cache.get(nextElement) == true)
+                break;
+        }
+
+        return index;
     }
 
     // Definition for singly-linked list.
