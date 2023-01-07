846bc210f361cc5c1b89f3619ce5f19ce2f1e254
sehun-kim <rogarithm@gmail.com>
2023-01-07 21:55:20 +0900

    ListNode 길이 계산 메서드가 순환 ListNode인지 확인하는 역할을 맡는다
     - 이를 위해 출력 타입을 boolean으로 변경했다.

--- src/main/java/leetcode/LinkedListCycle.java
+++ src/main/java/leetcode/LinkedListCycle.java
@@ -15,24 +15,23 @@ public class LinkedListCycle {
         LinkedListCycle tester = new LinkedListCycle();
         ListNode l1 = new ListNode(1);
         System.out.println(tester.hasCycle(l1) == false);
-        System.out.println(tester.getListNodeLength(l1));
+        System.out.println(tester.getListNodeLength(l1) == false);
 
         ListNode l2 = new ListNode(1);
         ListNode l2Next = new ListNode(2);
         l2.next = l2Next;
         l2Next.next = l1;
         System.out.println(tester.hasCycle(l2) == true);
-        System.out.println(tester.getListNodeLength(l2));
+        System.out.println(tester.getListNodeLength(l2) == true);
 
         ListNode l3 = new ListNode(1);
         ListNode l3Next = new ListNode(2);
         l3.next = l3Next;
         System.out.println(tester.hasCycle(l3) == false);
-        System.out.println(tester.getListNodeLength(l3));
+        System.out.println(tester.getListNodeLength(l3) == false);
     }
 
     public boolean hasCycle(ListNode head) {
-        int length = getListNodeLength(head);
         if (head.next == null) // 재귀함수로 만들 경우에도 이 조건이 유효한가? 유효하다. 유효한 순환 LinkedList는 모든 ListNode가 next 필드를 가져야 한다.
             return false;
         // 마지막 요소인지 판별할 수 있는 조건이 있나? 순회하는 요소 목록을 기록해야 할까?
@@ -44,7 +43,7 @@ public class LinkedListCycle {
             return true;
     }
 
-    private int getListNodeLength(ListNode head) {
+    private boolean getListNodeLength(ListNode head) {
         Map<ListNode, Boolean> cache = new HashMap<>();
         int index = 0;
 
@@ -64,7 +63,7 @@ public class LinkedListCycle {
                 break;
         }
 
-        return index;
+        return false;
     }
 
     // Definition for singly-linked list.
