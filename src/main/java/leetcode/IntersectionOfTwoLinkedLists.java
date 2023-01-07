fb133b6226ec299e547ee4eacdd124ce7fb3efce
sehun-kim <rogarithm@gmail.com>
2023-01-07 22:24:37 +0900

    초기 환경 설정

---
+++ src/main/java/leetcode/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+// Given the heads of two singly linked-lists headA and headB, return the node at which the two lists
+// intersect. If the two linked lists have no intersection at all, return null.
+// The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+// Note that the linked lists must retain their original structure after the function returns.
+
+public class IntersectionOfTwoLinkedLists {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        return null;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+}
