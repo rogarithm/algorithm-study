8b5e6332e69addaa4781e5381454924dfed1bf92
sehun-kim <rogarithm@gmail.com>
2023-01-07 22:14:34 +0900

    쓰지 않는 메서드 삭제 후 메서드 정의 이동

--- src/main/java/leetcode/LinkedListCycle.java
+++ src/main/java/leetcode/LinkedListCycle.java
@@ -15,35 +15,20 @@ public class LinkedListCycle {
         LinkedListCycle tester = new LinkedListCycle();
         ListNode l1 = new ListNode(1);
         System.out.println(tester.hasCycle(l1) == false);
-        System.out.println(tester.getListNodeLength(l1) == false);
 
         ListNode l2 = new ListNode(1);
         ListNode l2Next = new ListNode(2);
         l2.next = l2Next;
         l2Next.next = l2;
         System.out.println(tester.hasCycle(l2) == true);
-        System.out.println(tester.getListNodeLength(l2) == true);
 
         ListNode l3 = new ListNode(1);
         ListNode l3Next = new ListNode(2);
         l3.next = l3Next;
         System.out.println(tester.hasCycle(l3) == false);
-        System.out.println(tester.getListNodeLength(l3) == false);
     }
 
     public boolean hasCycle(ListNode head) {
-        if (head.next == null) // 재귀함수로 만들 경우에도 이 조건이 유효한가? 유효하다. 유효한 순환 LinkedList는 모든 ListNode가 next 필드를 가져야 한다.
-            return false;
-        // 마지막 요소인지 판별할 수 있는 조건이 있나? 순회하는 요소 목록을 기록해야 할까?
-        // 문제에서 제시하는 것처럼 인덱스 값을 추가할 수 있다면 마지막인지 판별할 수 있을 것 같다.
-        // 객체 주소가 같은지를 비교하면 값이 같더라도 다른 노드인지 확인할 수 있을 것 같다.
-        if (head.next.next == null)
-            return false;
-        else
-            return true;
-    }
-
-    private boolean getListNodeLength(ListNode head) {
         Map<ListNode, Boolean> cache = new HashMap<>();
         boolean result = false;
 
