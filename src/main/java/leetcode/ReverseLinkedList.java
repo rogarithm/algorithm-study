de0c54684395377fa8b0fa71f943aea173ef563d
sehun-kim <rogarithm@gmail.com>
2023-01-12 18:28:07 +0900

    초기 환경 설정

---
+++ src/main/java/leetcode/ReverseLinkedList.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+
+    }
+    
+    public ListNode reverseList(ListNode head) {
+        return null;
+    }
+
+    private static class ListNode {
+
+        int val;
+        leetcode.ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, leetcode.ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
