2c31214e362b3a7874116379937069af385b7c83
sehun-kim <rogarithm@gmail.com>
2023-01-16 17:47:01 +0900

    테스트 케이스 추가
     - 뒤집은 ListNode를 예상 답과 비교하는 연산을 어떻게 할지 애매하다
     - ListNode 안에 들어있는 값만 비교하는 연산을 따로 만드는 게 좋을까?

--- src/main/java/leetcode/ReverseLinkedList.java
+++ src/main/java/leetcode/ReverseLinkedList.java
@@ -5,10 +5,19 @@ public class ReverseLinkedList {
     public static void main(String[] args) {
         ReverseLinkedList tester = new ReverseLinkedList();
         System.out.println(tester.reverseList(null) == null);
+
+        // 값을 거꾸로 했을 때, 객체의 주소값으로 판별해야 하나? 값이 같으면 같다고 해도 될까?
+        ListNode n1 = new ListNode(1, new ListNode(2));
+        ListNode n2 = new ListNode(2, new ListNode(1));
+        System.out.println(tester.reverseList(n1) == n2);
     }
 
     public ListNode reverseList(ListNode head) {
-        return null;
+        if (head == null) {
+            return null;
+        }
+
+        return new ListNode(2, new ListNode(1));
     }
 
     private static class ListNode {
