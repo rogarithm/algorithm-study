ed5d352a94b288d90f7ee126d3d138ca5afe63e4
sehun-kim <rogarithm@gmail.com>
2023-01-17 18:53:40 +0900

    초기 환경 설정

---
+++ src/main/java/leetcode_medium/WordDictionary.java
@@ -0,0 +1,24 @@
+package leetcode_medium;
+
+public class WordDictionary {
+
+    public static void main(String[] args) {
+
+    }
+
+    // initialize the object
+    public WordDictionary() {
+
+    }
+
+    // Adds word to the data structure, it can be matched later.
+    public void addWord(String word) {
+
+    }
+
+    // Returns true if there is any string in the data structure that matches word or false otherwise.
+    // word may contain dots '.' where dots can be matched with any letter.
+    public boolean search(String word) {
+        return false;
+    }
+}
