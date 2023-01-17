c31fa32cfed92a1ae56c5ae0d81e6d26a0530273
sehun-kim <rogarithm@gmail.com>
2023-01-17 19:04:22 +0900

    테스트 통과할 수 있도록 구현, 구현 아이디어 추가

--- src/main/java/leetcode_medium/WordDictionary.java
+++ src/main/java/leetcode_medium/WordDictionary.java
@@ -1,5 +1,8 @@
 package leetcode_medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordDictionary {
 
     public static void main(String[] args) {
@@ -8,19 +11,23 @@ public class WordDictionary {
         System.out.println(tester.search("bad") == true);
     }
 
-    // initialize the object
-    public WordDictionary() {
+    private Map<String, Boolean> dictionary;
 
+    // 사전으로 쓸 자료 구조를 초기화
+    public WordDictionary() {
+        dictionary = new HashMap<>();
     }
 
     // Adds word to the data structure, it can be matched later.
     public void addWord(String word) {
-
+        // . 문자를 입력했을 때 검색을 효율적으로 하려면 어떤 자료구조를 쓰는 게 좋을까?
+        dictionary.put(word, true);
     }
 
-    // Returns true if there is any string in the data structure that matches word or false otherwise.
-    // word may contain dots '.' where dots can be matched with any letter.
+    // 입력 문자열과 완전히 동일하거나 입력의 '.' 문자 이외 모든 문자가 동일한 단어가 사전에 있으면 true를 반환
     public boolean search(String word) {
+        if (dictionary.get(word) == true)
+            return true;
         return false;
     }
 }
