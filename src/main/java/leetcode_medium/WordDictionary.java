0f2f0f62840604899712843b0c5972926ebd72be
sehun-kim <rogarithm@gmail.com>
2023-01-20 20:55:58 +0900

    . 문자가 여러 개인 경우를 대비해 ArrayList에 저장

--- src/main/java/leetcode_medium/WordDictionary.java
+++ src/main/java/leetcode_medium/WordDictionary.java
@@ -1,6 +1,8 @@
 package leetcode_medium;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WordDictionary {
@@ -36,13 +38,18 @@ public class WordDictionary {
     // 만약 word에 . 문자가 포함되어 있다면, . 문자에 가능한 모든 문자를 대입해 검색한 결과 중 true가 반환되는 경우가 있는지를 확인해보면 될 것 같다.
     // . 문자가 여러 번 나올 경우, 계산이 복잡해지지 않게 하려면 어떻게 해야할까? (문제에서 . 문자는 최대 세 번 나온다고 한다)
     public boolean search(String word) {
+        List<Integer> dots = new ArrayList<>();
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == '.') {
+                dots.add(i);
+            }
+        }
 
-        if (word.contains(".")) {
-            int dotPosition = word.indexOf(".");
+        if (!dots.isEmpty() && dots.size() == 1) {
             char[] alpha = "abcdefghijklmnopqrstuvwxyz".toCharArray(); // https://stackoverflow.com/a/17575926
             for (char character : alpha) {
                 String candidate =
-                        word.substring(0, dotPosition) + character + word.substring(dotPosition + 1);
+                        word.substring(0, dots.get(0)) + character + word.substring(dots.get(0) + 1);
                 if (dictionary.get(candidate) != null && dictionary.get(candidate) == true) {
                     return true;
                 }
