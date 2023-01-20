590862dabf7dc46f2152d1462462a57b8aae9386
sehun-kim <rogarithm@gmail.com>
2023-01-20 20:33:41 +0900

    쓸모 없어진 학습 테스트를 지운다

--- src/test/java/jdk/StringTest.java
+++ src/test/java/jdk/StringTest.java
@@ -6,29 +6,6 @@ import org.junit.jupiter.api.Test;
 
 public class StringTest {
 
-    public static void main(String[] args) {
-
-        for (String character : new String[]{"a", "b", "c"}) {
-            System.out.println(character);
-        }
-
-        System.out.println("bad".contains("."));
-        System.out.println(".ad".contains("."));
-        System.out.println("a.d".contains("."));
-
-        System.out.println("bad".indexOf(".") == -1);
-        System.out.println(".ad".indexOf(".") == 0);
-        System.out.println("a.d".indexOf(".") == 1);
-
-        String word = "a.cd";
-        int dotPosition = word.indexOf(".");
-        for (String character : new String[]{"a", "b", "c"}) {
-            System.out.println(word.substring(0, dotPosition)
-                    + character
-                    + word.substring(dotPosition + 1, word.length()));
-        }
-    }
-
     @Test
     public void testSubstringSecondArgIsExclusive() {
         String word = "a.cd";
