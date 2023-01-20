2d0f4de3a2fd4315d7342077565b37e2f3070b62
sehun-kim <rogarithm@gmail.com>
2023-01-20 20:32:27 +0900

    main에 모여있는 학습 테스트를 단위 테스트로 쪼갠다

--- src/test/java/jdk/StringTest.java
+++ src/test/java/jdk/StringTest.java
@@ -1,6 +1,9 @@
 package jdk;
 
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class StringTest {
 
     public static void main(String[] args) {
@@ -24,6 +27,54 @@ public class StringTest {
                     + character
                     + word.substring(dotPosition + 1, word.length()));
         }
+    }
+
+    @Test
+    public void testSubstringSecondArgIsExclusive() {
+        String word = "a.cd";
+        int dotPosition = word.indexOf(".");
+        Assertions.assertThat(word.substring(0, dotPosition)).isEqualTo("a");
+    }
+
+    @Test
+    public void testSubstringSecondArgNoNeedIfItIsEndOfString() {
+        String word = "abcd";
+        Assertions.assertThat(word.substring(1, word.length())).isEqualTo(word.substring(1));
+    }
+
+    @Test
+    public void testConcatSubstring() {
+        String word = "a.cd";
+        int dotPosition = word.indexOf(".");
+        String character = "b";
+        Assertions.assertThat(
+                word.substring(0, dotPosition)
+                        + character
+                        + word.substring(dotPosition + 1))
+                .isEqualTo("abcd");
+    }
+
+    @Test
+    public void testIndexOfFail() {
+        String word = "abcd";
+        Assertions.assertThat(word.indexOf(".")).isEqualTo(-1);
+    }
+
+    @Test
+    public void testIndexOfSuccess() {
+        String word = "a.cd";
+        Assertions.assertThat(word.indexOf(".")).isEqualTo(1);
+    }
+
+    @Test
+    public void testContainsSuccess() {
+        Assertions.assertThat(".ad".contains(".")).isEqualTo(true);
+        Assertions.assertThat("a.d".contains(".")).isEqualTo(true);
+    }
+
+    @Test
+    public void testContainsFail() {
+        Assertions.assertThat("bad".contains(".")).isEqualTo(false);
     }
 
 }
