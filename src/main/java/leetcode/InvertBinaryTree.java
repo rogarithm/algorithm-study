be255578f355bf7a63eddbd0040e9b29ca2661ee
sehun-kim <rogarithm@gmail.com>
2023-01-24 22:50:11 +0900

    테스트 데이터 추가

--- src/main/java/leetcode/InvertBinaryTree.java
+++ src/main/java/leetcode/InvertBinaryTree.java
@@ -7,6 +7,14 @@ public class InvertBinaryTree {
         System.out.println(tester.invertTree(null) == null);
         System.out.println(tester.invertTree(new TreeNode(2, new TreeNode(1),new TreeNode(3))).toString()
                 .equals(new TreeNode(2, new TreeNode(3), new TreeNode(1)).toString()));
+        TreeNode t3 = new TreeNode(4,
+                new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+                new TreeNode(7, new TreeNode(6), new TreeNode(9)));
+        TreeNode t3inverted = new TreeNode(4,
+                new TreeNode(7, new TreeNode(6), new TreeNode(9)),
+                new TreeNode(2, new TreeNode(1), new TreeNode(3))
+        );
+        System.out.println(tester.invertTree(t3).toString().equals(t3inverted.toString()));
     }
 
     public TreeNode invertTree(TreeNode root) {
