3e902c1a20548a1aa0644fa3982b3e00ad348ce9
sehun-kim <rogarithm@gmail.com>
2023-02-15 17:08:11 +0900

    char 클래스 학습 테스트 추가

---
+++ src/test/java/jdk/CharacterTest.java
@@ -0,0 +1,53 @@
+package jdk;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CharacterTest {
+
+    public static void main(String[] args) {
+        hackerrank.CeasarCipher tester = new hackerrank.CeasarCipher();
+        System.out.println(tester.caesarCipher("middle-Outz", 1));
+    }
+
+    @Test
+    @DisplayName("String을 char 배열로 변환할 수 있다")
+    public void testConvertStringToCharArray() {
+        String s = "string";
+        char[] chars = s.toCharArray();
+        assertThat(chars[0]).isEqualTo('s');
+    }
+
+    @Test
+    @DisplayName("StringBuilder에 char 타입 값을 덧붙일 수 있다")
+    public void testAppendCharToStringBuilder() {
+        String s = "string";
+        char[] chars = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char c : chars) {
+            sb.append(c);
+        }
+        assertThat(sb.toString()).isEqualTo("string");
+    }
+
+    @Test
+    @DisplayName("char 타입 범위 내라면 int 타입 값을 char 타입으로 변환해 쓸 수 있다")
+    public void testConvertIntToCharOrViceVersa() {
+        char smallA = 'a';
+        int smallAInInt = 97;
+        assertThat(smallA).isEqualTo((char)smallAInInt);
+        assertThat((int)smallA).isEqualTo(smallAInInt);
+    }
+
+    @Test
+    @DisplayName("char 타입 값을 비교할 때 정수 비교 연산자를 쓸 수 있다")
+    public void testComparingChars() {
+        char c = 'c';
+        char f = 'f';
+        assertThat(c >= 'a' && c <= 'e').isTrue();
+        assertThat(f >= 'a' && f <= 'e').isFalse();
+    }
+
+}
