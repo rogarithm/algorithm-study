a508e8118b86ef5c5a0da48dcb8d0a6d604a4aeb
sehun-kim <rogarithm@gmail.com>
2023-02-15 17:05:29 +0900

    ArrayList set 메서드 학습 테스트 추가

--- src/test/java/jdk/ArrayListTest.java
+++ src/test/java/jdk/ArrayListTest.java
@@ -1,10 +1,13 @@
 package jdk;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ArrayListTest {
+
     @Test
     public void testArrayListChangesSizeWhenAddElements() {
         ArrayList<Integer> arrayList = new ArrayList<>();
@@ -14,4 +17,28 @@ public class ArrayListTest {
         Assertions.assertEquals(2, arrayList.size());
     }
 
+    @Test
+    public void testSet() {
+        //ArrayList<Integer> test = new ArrayList<>(Arrays.asList(1, 1, 1, 2, 3));
+        //ArrayList<Integer> test = new ArrayList<>(Arrays.asList(0, 1, 1, 2, 4));
+        ArrayList<Integer> test = new ArrayList<>(Arrays.asList(1, 1, 2, 3, 1));
+        List<Integer> counts = new ArrayList<Integer>();
+        int testSize = test.size();
+
+        for (int i = 0; i < testSize; i++) {
+            counts.add(i, 0);
+        }
+
+        for (int i = 0; i < testSize; i++) {
+            int index = test.get(i);
+            int currentCount = counts.get(index);
+
+            System.out.println("index: " + index + ", current count: " + currentCount);
+            counts.set(index, currentCount + 1); //index, value
+        }
+
+        for (int elem : counts) {
+            System.out.println("E: " + elem);
+        }
+    }
 }
