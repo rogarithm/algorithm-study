eb79aabe380866fd31ea1c10357e2497e8bc1272
sehun-kim <rogarithm@gmail.com>
2023-02-20 21:29:05 +0900

    구현 아이디어 추가

--- src/main/java/leetcode/medium/LongestStringChain.java
+++ src/main/java/leetcode/medium/LongestStringChain.java
@@ -12,8 +12,9 @@ public class LongestStringChain {
     }
 
     //주어진 배열을 정렬한 뒤에 시작하는 것이 좋을 것 같다. String[] 타입을 어떻게 정렬하지?
-    //오름차순 vs 내림차순
+    // 오름차순 vs 내림차순
     //앞의 요소가 뒤의 요소에서 한 글자를 빼서 만들어질 수 있는지 확인
+    // 가능한 한 글자씩 빼보는 걸 어떻게 해봐야할까?
     //동일한 길이의 요소가 여러 가지이고 그 중 일부만 다음 길이 요소의 predecessor가 될 수 있는 경우는 어떻게 처리해야할까?
     public int longestStrChain(String[] words) {
         if (words.length == 0) {
