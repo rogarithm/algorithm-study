043dc03c1f7d11f4b4e223f4b86bace79d21bdce
sehun-kim <rogarithm@gmail.com>
2023-02-20 21:28:49 +0900

    테스트 케이스 추가

--- src/main/java/leetcode/medium/LongestStringChain.java
+++ src/main/java/leetcode/medium/LongestStringChain.java
@@ -6,7 +6,9 @@ public class LongestStringChain {
 
     public static void main(String[] args) {
         LongestStringChain tester = new LongestStringChain();
+        System.out.println(tester.longestStrChain(new String[]{"a"}) == 1);
         System.out.println(tester.longestStrChain(new String[]{"a", "ab"}) == 2);
+        System.out.println(tester.longestStrChain(new String[]{"a", "ab", "abc"}) == 3);
     }
 
     //주어진 배열을 정렬한 뒤에 시작하는 것이 좋을 것 같다. String[] 타입을 어떻게 정렬하지?
@@ -14,7 +16,15 @@ public class LongestStringChain {
     //앞의 요소가 뒤의 요소에서 한 글자를 빼서 만들어질 수 있는지 확인
     //동일한 길이의 요소가 여러 가지이고 그 중 일부만 다음 길이 요소의 predecessor가 될 수 있는 경우는 어떻게 처리해야할까?
     public int longestStrChain(String[] words) {
-        return 2;
+        if (words.length == 0) {
+            return 0;
+        }
+
+        if (words[1].substring(0,1).equals(words[0])
+                || words[1].substring(1, words.length).equals(words[0]))
+            return 2;
+        else
+            return 1;
     }
 
 }
