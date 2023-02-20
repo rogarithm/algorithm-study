9d82b46554242269b1849df9432a78326b07d694
sehun-kim <rogarithm@gmail.com>
2023-02-20 17:58:30 +0900

    초기 설정

---
+++ src/main/java/leetcode/medium/LongestStringChain.java
@@ -0,0 +1,15 @@
+package leetcode.medium;
+
+// 1048. LongestStringChain
+// https://leetcode.com/problems/longest-string-chain/
+public class LongestStringChain {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int longestStrChain(String[] words) {
+        return 0;
+    }
+
+}
