76dc7cd650b27a524f830c0b72cd1e9a53397f65
sehun-kim <rogarithm@gmail.com>
2023-02-24 20:10:23 +0900

    trie 자료구조 구현 할일 목록 및 테스트 추가

---
+++ src/test/java/datastructure/TrieTest.java
@@ -0,0 +1,65 @@
+package datastructure;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TrieTest {
+
+    @Test
+    @DisplayName("문자 하나를 trie에 추가할 수 있다")
+    public void addOneCharToTrie() {
+        Trie trie = new Trie();
+        trie.addWord("a");
+        Assertions.assertThat(trie.search("a")).isTrue();
+    }
+
+    @Test
+    @DisplayName("문자 여러개를 trie에 추가할 수 있다")
+    public void addMultipleCharsToTrie() {
+        Trie trie = new Trie();
+        trie.addWord("ab");
+        Assertions.assertThat(trie.search("ab")).isTrue();
+    }
+
+    private static class Trie {
+
+        char currentChar;
+        TreeNode words;
+        boolean isWord;
+
+        public Trie() {
+        }
+
+        public void addWord(String s) {
+            char[] chars = s.toCharArray();
+            for (char c : chars) {
+                this.currentChar = c;
+            }
+        }
+
+        public boolean search(String s) {
+            return true;
+        }
+    }
+
+    private static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
