06e09742af60c3a5a882fe51e8013d20e05b7950
sehun-kim <rogarithm@gmail.com>
2023-03-03 19:31:16 +0900

    2차원 배열 학습 테스트

---
+++ src/test/java/jdk/ArrayTest.java
@@ -0,0 +1,37 @@
+package jdk;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ArrayTest {
+
+    @Test
+    public void construct2DArray() {
+        int[][] twoDim = {{1,2,3},{4,5,6}};
+
+        int[][] twoDim2 = new int[2][3];
+        twoDim2[0][0] = 1;
+        twoDim2[0][1] = 2;
+        twoDim2[0][2] = 3;
+        twoDim2[1][0] = 4;
+        twoDim2[1][1] = 5;
+        twoDim2[1][2] = 6;
+    }
+
+    @Test
+    public void getOneDimensionLengthFromTwoDimensionArray() {
+        int[][] twoDim = {{1,2,3},{4,5,6}};
+        Assertions.assertThat(twoDim.length).isEqualTo(2);
+    }
+
+    @Test
+    public void getTwoDimensionLengthFromTwoDimensionArray() {
+        int[][] twoDim = {{1,2,3},{4,5,6}};
+        Assertions.assertThat(twoDim[0].length).isEqualTo(3);
+        Assertions.assertThat(twoDim[1].length).isEqualTo(3);
+    }
+
+
+
+}
