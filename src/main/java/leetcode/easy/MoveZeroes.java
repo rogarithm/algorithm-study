a0ee40e43e356ec8e4a42f75d38b1c24d325122f
sehun-kim <rogarithm@gmail.com>
2023-03-09 00:16:01 +0900

    실패 케이스 처리
     - 0 갯수가 1개 이상일 때만 배열 요소의 위치를 바꾸도록 수정했다.

--- src/main/java/leetcode/easy/MoveZeroes.java
+++ src/main/java/leetcode/easy/MoveZeroes.java
@@ -33,7 +33,7 @@ public class MoveZeroes {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0)
                 zeroCount++;
-            else {
+            else if (zeroCount > 0) {
                 nums[i - zeroCount] = nums[i];
                 nums[i] = 0;
             }
