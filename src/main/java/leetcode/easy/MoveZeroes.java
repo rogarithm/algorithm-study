296387f6d521cde815457da88e3adeaa82cd25c2
sehun-kim <rogarithm@gmail.com>
2023-03-08 23:59:28 +0900

    뒤에서부터 바꾸도록 구현. 실패

--- src/main/java/leetcode/easy/MoveZeroes.java
+++ src/main/java/leetcode/easy/MoveZeroes.java
@@ -17,24 +17,31 @@ public class MoveZeroes {
         int[] t3 = {0, 0, 1};
         tester.moveZeroes(t3);
         tester.printArray(t3);
+        int[] t4 = {0, 1, 0, 3};
+        tester.moveZeroes(t4);
+        tester.printArray(t4);
     }
 
     //0이 맨 앞에 두 개 나오면, 맨 앞의 0이 맨 뒤로 보내진 다음 '맨 앞'에 0이 남아있다.
     //인덱스가 1이 되기 때문에, 새로 맨 앞에 위치하게 된 0을 뒤로 보낼 수가 없게 된다.
     public void moveZeroes(int[] nums) {
 
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (i >= 1 && nums[i - 1] == 0) {
-                i -= 1;
-            }
-            if (nums[i] == 0) {
-                for (int j = i; j < nums.length - 1; j++) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
+        int zeroCount = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == 0) zeroCount++;
+        }
+
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (i == nums.length - 2 && nums[i + 1] != 0) i++;
+            if (zeroCount - 1 == i) break;
+            if (nums[i] == 0) continue;
+            for (int j = i; j > 0; j--) {
+                int temp = nums[j];
+                nums[j] = nums[j - 1];
+                nums[j - 1] = temp;
             }
         }
+
 
     }
 
