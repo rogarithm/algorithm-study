30760fd95f018674cab60e8444a7b68ec816791e
sehun-kim <rogarithm@gmail.com>
2023-03-22 01:08:31 +0900

    ##prefixsum 알고리즘 학습 테스트 #릿코드 통과 #Merge pull request #30 from rogarithm/leetcode/medium-corporate-flight-bookings 초기 설정 ##릿코드 통과 #Merge pull request #30 from rogarithm/leetcode/medium-corporate-flight-bookings ##prefixsum 알고리즘 학습 테스트 #릿코드 통과 #Merge pull request #30 from rogarithm/leetcode/medium-corporate-flight-bookings 초기 설정 학습 테스트 추가 ##Merge pull request #30 from rogarithm/leetcode/medium-corporate-flight-bookings ##prefixsum 알고리즘 학습 테스트 #릿코드 통과 #Merge pull request #30 from rogarithm/leetcode/medium-corporate-flight-bookings 초기 설정 ##릿코드 통과 #Merge pull request #30 from rogarithm/leetcode/medium-corporate-flight-bookings ##prefixsum 알고리즘 학습 테스트 #릿코드 통과 #Merge pull request #30 from rogarithm/leetcode/medium-corporate-flight-bookings 초기 설정 학습 테스트 추가 문제 풀이 중간 과정 학습 테스트 추가 문제 풀이. 실패
     - result에 포함한 강의를 prerequisite으로 하는 다른 강의를
       prerequisiteLists에서 찾아서 그 강의의 indegree 값을 1씩
       빼야 하는데 해당 강의를 찾을 수 없게 prerequisiteLists가
       만들어진 상태다.
     - 해결하기 위해서는, prerequisiteLists의 key와 value에 들어갈
       pre[]를 반대로 해야 한다.

--- src/main/java/leetcode/medium/CourseScheduleII.java
+++ src/main/java/leetcode/medium/CourseScheduleII.java
@@ -1,10 +1,63 @@
 package leetcode.medium;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 public class CourseScheduleII {
 
+    public static void main(String[] args) {
+        int[][] t = new int[][]{{1,0},{2,0},{3,1},{3,2}};
+
+        CourseScheduleII tester = new CourseScheduleII();
+        int[] result = tester.findOrder(4, t);
+        //for (int i : result) {
+        //    System.out.println(i);
+        //}
+    }
+
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        return new int[]{};
+        int[] result = new int[numCourses];
+        int idx = 0;
+        int[] indegrees = new int[numCourses];
+        for (int[] pre : prerequisites) {
+            indegrees[pre[0]] += 1;
+            //System.out.println("ind: " + indegrees[pre[0]]);
+        }
+
+        Map<Integer, Set<Integer>> prerequisiteLists = new HashMap<>();
+        for (int[] pre : prerequisites) {
+            prerequisiteLists.putIfAbsent(pre[0], new HashSet<>());
+            prerequisiteLists.get(pre[0]).add(pre[1]);
+            //System.out.println("pre list: ");
+            //prerequisiteLists.get(pre[0]).forEach(e -> System.out.println(e + ", "));
+        }
+
+        Queue<Integer> preResult = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegrees[i] == 0) {
+                preResult.add(i);
+            }
+        }
+
+        while (!preResult.isEmpty()) {
+            Integer currentCourse = preResult.poll();
+            result[idx++] = currentCourse;
+            System.out.println("currentCourse: " + currentCourse);
+
+            if (prerequisiteLists.containsKey(currentCourse)) {
+                for (int pre : prerequisiteLists.get(currentCourse)) {
+                    indegrees[pre] -= 1;
+                    if (indegrees[pre] == 0) {
+                        preResult.add(pre);
+                    }
+                }
+            }
+        }
+
+        return idx == numCourses ? result : new int[]{};
     }
 }
