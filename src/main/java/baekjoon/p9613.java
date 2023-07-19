4f0d9b133a963f144d355ddd25ac49c73b9c9194
sehun-kim <rogarithm@gmail.com>
2023-07-19 15:26:26 +0900

    9613

--- src/main/java/baekjoon/p9613.java
+++ src/main/java/baekjoon/p9613.java
@@ -2,63 +2,42 @@ package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.StringReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class p9613 {
 
     public static void main(String[] args) throws IOException {
-        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader br = new BufferedReader(new StringReader("1\n"
-                + "4 2 4 6 8"));
-        int count = Integer.parseInt(br.readLine());
-        List<Integer> elems = new ArrayList<>();
-        List<List<Pair>> combinations = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cases = Integer.parseInt(br.readLine());
 
-        for (int i=0; i<count; i++) {
+        for (int i=0; i<cases; i++) {
+            List<Integer> elems = new ArrayList<>();
             String[] testCase = br.readLine().split(" ");
-            int nums = Integer.parseInt(testCase[0]);
-            for (int j=0; j<nums; j++) {
+            int count = Integer.parseInt(testCase[0]);
+            for (int j=0; j<count; j++) {
                 elems.add(Integer.parseInt(testCase[j+1]));
             }
 
-            for (int k=0; k<nums; k++) {
-                List<Pair> combination = new ArrayList<>();
+            Long gcdSum = 0L;
+            for (int k=0; k<count; k++) {
                 Integer left = elems.get(k);
-                for (int l=k+1; l<nums; l++) {
+                for (int l=k+1; l<count; l++) {
                     Integer right = elems.get(l);
-                    combination.add(new Pair(left, right));
+                    gcdSum += gcd(left, right);
                 }
-                combinations.add(combination);
             }
-        }
-
-        int c = 0;
-        for (List<Pair> comb : combinations) {
-            System.out.print(c + ": ");
-            for (Pair pair : comb) {
-                System.out.print(pair + ", ");
-            }
-            System.out.println();
-            c++;
+            System.out.println(gcdSum);
         }
     }
 
-    private static class Pair {
+    static long gcd(int m, int n) {
+        int tmp = m;
+        m = Math.max(tmp, n);
+        n = Math.min(tmp, n);
 
-        int left;
-        int right;
-
-        public Pair(Integer left, Integer right) {
-            this.left = left;
-            this.right = right;
-        }
-
-        @Override
-        public String toString() {
-            return "(" + left + "," + right + ")";
-        }
+        if (m % n == 0) return n;
+        return gcd(n, m % n);
     }
-
 }
