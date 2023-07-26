6f5a9cbb0f9c6005a7cf3b4661aadb1f7615998d
sehun-kim <rogarithm@gmail.com>
2023-07-26 22:11:55 +0900

    6588 시간초과

---
+++ src/main/java/baekjoon/p6588.java
@@ -0,0 +1,56 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+public class p6588 {
+
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new StringReader("8\n"
+                + "20\n"
+                + "42\n"
+                + "0"));
+        while (true) {
+            int max = Integer.MIN_VALUE;
+            int a = 0;
+            int b = 0;
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) break;
+            for (int i=3; i<num; i=i+2) {
+                boolean isPrime = true;
+                for (int j=2; j<Math.sqrt(i); j++) {
+                    if (i%j==0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (!isPrime) continue;
+
+                isPrime = true;
+                for (int j=2; j<Math.sqrt(num-i); j++) {
+                    if (i%j==0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (!isPrime) continue;
+
+                if (num-i - i > max) {
+                    a = i;
+                    b = num - i;
+                    max = num - 2*i;
+                }
+            }
+
+            if (a==0 && b==0)
+                System.out.println("Goldbach's conjecture is wrong.");
+            else
+                System.out.println(num + " = " + a + " + " + b);
+        }
+    }
+
+}
